aec95464f3db2473526a27794af86bdfa0b89519
quch1012 <quch1012@h-ka.de>
2022-10-17 08:37:53 +0200

    correct freq tb and uniques to predict accordingly, allow no prediction on missing information, add authorkit for documentation

--- helperFunctions.py
+++ helperFunctions.py
@@ -3,53 +3,18 @@ import string
 import pandas as pd
 import nltk
 from nltk.corpus import stopwords
+from nltk import ngrams
 
-def readDataset(datapath: str):
-    # Get the file details
-    directory = []
-    file = []
-    title = []
-    text = []
-    label = []
-    for dirname, _ , filenames in os.walk(datapath):
-        print('Directory: ', dirname)
-        print('Subdir: ', dirname.split('/')[-1])
-
-        # traverse direcotries
-        for filename in filenames:
-            directory.append(dirname)
-            file.append(filename)
-            label.append(dirname.split('/')[-1])
-            fullpathfile = os.path.join(dirname,filename)
-
-            # read files
-            with open(fullpathfile, 'r', encoding="utf8", errors='ignore') as infile:
-                intext = ''
-                firstline = True
-                for line in infile:
-                    if firstline:
-                        title.append(line.replace('\n',''))
-                        firstline = False
-                    else:
-                        intext = intext + ' ' + line.replace('\n','')
-                text.append(intext)
-    
-    # create dataframe    
-    fulldf = pd.DataFrame(list(zip(directory, file, title, text, label)), 
-        columns =['directory', 'file', 'title', 'text', 'label'])
-
-    return fulldf.filter(['text','label'], axis=1)
-
-def cleanNLFeature(text: str):
+def preprocessNLFeature(text: str):
     # load symbols and stopwords
     nltk.download('punkt')
     nltk.download("stopwords")
     stop = set(nltk.corpus.stopwords.words("english"))
     s =  set(string.punctuation)
 
-    tok = nltk.word_tokenize(text)
+    tokens = nltk.word_tokenize(text)
     # remove stopwords, punctuation
-    filtered_words = [word.lower() for word in tok if word.lower() not in stop and word.lower() not in s]
+    filtered_words = [word.lower() for word in tokens if word.lower() not in stop and word.lower() not in s]
     # remove digits
     filtered_words = [word.replace('\d+', '') for word in filtered_words]
     # remove one-char tokens
@@ -81,3 +46,5 @@ def cleanNLFeature(text: str):
     text = ' '.join(word for word in filtered_words)  """
     return text
 
+def createNgrams(text: str, n):
+    return list(ngrams(text.split(), n))
--- naiveBayes.py
+++ naiveBayes.py
@@ -1,15 +1,15 @@
 import numpy as np
 from pandas import DataFrame, Series
 
-# returns prediction of truth of a event(word) following a specified hypothesis(class).
+
+# returns prediction of truth of an event(instance to be classified) following a specified hypothesis(class).
 # If hypothesis is None, the probabilities for all hypothesies preceding the event will be calculated
-def predict(uniques: list, freq_tb: np.ndarray, sumRowsRel: np.ndarray, sumColsRel: np.ndarray, event: str, hypothesis: np.int64 = None ):
+def predict(uniques: list, freq_tb: np.ndarray, sumRowsRel: np.ndarray, sumColsRel: np.ndarray, event, hypothesis: np.int64 = None, classes: range = range(2)):
         if event not in uniques:
                 #print(event + ' not in vocabulary!')
                 return
         if hypothesis == None:
-                return (predict(uniques, freq_tb, sumRowsRel, sumColsRel, event, 0),
-                        predict(uniques, freq_tb, sumRowsRel, sumColsRel, event, 1))
+                return (predict(uniques, freq_tb, sumRowsRel, sumColsRel, event, i) for i in classes)
         # P(hypothesis|event) = (P(event|hypothesis) * P(hypothesis)) / P(event)
         i = uniques.index(event)
         p_h_e = freq_tb[i][hypothesis] / np.sum(freq_tb, axis=0)[hypothesis]
@@ -17,37 +17,68 @@ def predict(uniques: list, freq_tb: np.ndarray, sumRowsRel: np.ndarray, sumColsR
         p_e = sumColsRel[hypothesis]
         return p_h_e * p_h / p_e
 
-def predictDoc(document: str, uniques: list, freq_tb: np.ndarray, sumRowsRel: np.ndarray, sumColsRel: np.ndarray):
-    # probabilities for a word to belong to a class (for every class)
+def predictDoc(document: np.iterable, uniques: list, freq_tb: np.ndarray, sumRowsRel: np.ndarray, sumColsRel: np.ndarray):
+    # probabilities for an instance to classify to belong to a class (for every class)
     documentsProbs = []
-    for word in document.split():
-        p = predict(uniques, freq_tb, sumRowsRel, sumColsRel, word)
+    for iter in document:
+        p = predict(uniques, freq_tb, sumRowsRel, sumColsRel, iter)
         if p is not None:
             documentsProbs.append( [i for i in p] ) 
     
+    # empty list, no prediction could be made
+    if not documentsProbs:
+        return -1
     # sum up the probabilities
     return np.argmax(np.sum(documentsProbs, axis=0))
 
-def frequencyTable(df: DataFrame):
+def frequencyTableOccurences(toCount: Series, labels: Series):
     # get the complete vocabulary
-    stack = df['text'].str.split(' ', expand=True).stack()
+    stack = toCount.str.split(' ', expand=True).stack()
     uniques = list(stack.unique())
 
     # count words in a document
-    counts = df['text'].apply(lambda x: np.unique(x.split(' '), return_counts=True))
+    counts = toCount.apply(lambda x: np.unique(x.split(' '), return_counts=True))
+    
+    # sadly, the indices don't match between the NB variants -> code replication with altered indices
     # init frequency table with vocab in rows and labels as columns
-    freq_tb = np.zeros((len(uniques), df.label.nunique()))
-
-    # iterate through all documents word counts and fill the freq table
+    freq_tb = np.zeros((len(uniques), labels.nunique()))
+    
+    # iterate through all documents instances to count and fill the freq table
     # foreach document
     for i in range(len(counts)):
-        # foreach word in document
+        # foreach instance to count in the document
         for j in range(len(counts[i][0])):
-            # word exists in vocabulary (may not be the case due to train val test split)
+            # instance to count exists in vocabulary (may not be the case due to train val test split)
             if counts[i][0][j] in uniques:
-                # update table on corresponding index [wordindex][labelindex]
-                freq_tb[uniques.index(counts[i][0][j])][df.label[i]] += counts[i][1][j]
+                # update table on corresponding index [instances-to-count-index][labelindex]
+                freq_tb[uniques.index(counts[i][0][j])][labels[i]] += counts[i][1][j]
     return freq_tb, uniques
+    
+def frequencyTableNgrams(toCount: Series, labels: Series):
+    # get the complete vocabulary
+    uniques = set()
+    for ngramlst in toCount[:5]:
+        for st in ngramlst:
+            uniques.add(st)
+    uniques = list(uniques)
+
+    # count occurance of each n-gram of a document
+    counts = toCount.apply(lambda lstOfSets: [(_set, lstOfSets.count(_set)) for _set in sorted(set(lstOfSets))])
+
+    # sadly, the indices don't match between the NB variants -> code replication with altered indices
+    freq_tb = np.zeros((len(uniques), 2))
+    # iterate through all documents instances to count and fill the freq table
+    # foreach document
+    for i in range(len(counts)):
+        # foreach instance to count in the document
+        for j in range(len(counts[i])):
+            # instance to count exists in vocabulary (may not be the case due to train val test split)
+            if counts[i][j][0] in uniques:
+                # update table on corresponding index [instances-to-count-index][labelindex]
+                toAdd = counts[i][j][1]
+                freq_tb[uniques.index(counts[i][j][0])][labels[i]] += toAdd
+
+    return freq_tb, uniques    
 
 def likelihoodTable(freq_tb: np.array):
     # total, row and columns wordcount
